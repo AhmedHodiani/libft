e0e57ae49c7e2543f5b189969b260003ba288064
Mohammed Darawsheh <mdarawsh@c2r1s7.42amman.com>
2024-09-05 16:39:30 +0300

    fix: substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mdarawsh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:21:25 by mdarawsh          #+#    #+#             */
-/*   Updated: 2024/09/02 15:51:38 by mdarawsh         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:37:06 by mdarawsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,34 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	size_t	i;
 
-	if (s[start] == '\0' || len == 0)
+	if (start >= ft_strlen(s))
 	{
-		return (NULL);
-	}
-	if ((ft_strlen(s) - start) >= len)
-	{
-		ptr = malloc(len);
-		while (len--)
-		{
-			*ptr = s[start];
-			ptr++;
-			start++;
-		}
+		ptr = malloc(1);
+		*ptr = '\0';
 		return (ptr);
 	}
-	else
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	ptr = malloc(len + 1);
+	if (ptr == NULL || s == NULL)
 		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
 }
+//      s       start           len 
+
+//      ""        1               0   
+
+//      ""        1               1    
+
+//      ""        0               0    
+
+//      ""        0               1  
