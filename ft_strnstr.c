be2330eef93fe3a0ed4c692a661e6e1ae033e4b6
Mohammed Darawsheh <mdarawsh@c2r1s3.42amman.com>
2024-09-05 12:49:09 +0300

    fix: strnstr but we have many crashs

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mdarawsh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 18:02:37 by mdarawsh          #+#    #+#             */
-/*   Updated: 2024/09/01 18:58:46 by mdarawsh         ###   ########.fr       */
+/*   Updated: 2024/09/05 12:41:52 by mdarawsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,21 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
 
-	if (little == NULL)
+	if (little == NULL || little[0] == '\0')
 		return ((char *)big);
+	if (big == NULL || len == 0 || big [0] == '\0')
+		return (NULL);
 	i = 0;
-	while (len--)
+	while (i < len && big[i] != '\0')
 	{
 		j = 0;
-		while (little[j])
-		{
-			if (big[i + j] == little[j])
-				j++;
-			if (little[++j] == '\0')
-				return ((char *)big + i);
-			else
-				break ;
-		}
+		while (big[i + j] == little[j] && i + j < len && little[j] != '\0')
+			j++;
+		if (little[j] == '\0')
+			return ((char *)(big + i));
 		i++;
 	}
 	return (NULL);
